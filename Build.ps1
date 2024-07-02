16d12a3724c51c52528484464efc1d168e819e70
a1ex9788 <alejandrocarrion1999@gmail.com>
2024-07-02 16:17:46 +0200

    Feat: Crear proyecto para generar paquete NuGet.

--- Build.ps1
+++ Build.ps1
@@ -1,22 +1,26 @@
 param (
     [switch]$Build,
     [switch]$Test,
+    [switch]$Publish,
 
     [string]$Configuration = "Release",
-    [string]$TestResultsDirectory = "TestResults"
+    [string]$TestResultsDirectory = "TestResults",
+    [string]$PublicationDirectory = "Publish",
+    [string]$Version = "9.9"
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-if ((-not $Build) -and (-not $Test))
+if ((-not $Build) -and (-not $Test) -and (-not $Publish))
 {
-    Write-Warning "Activate any flag (-Build | -Test) to perform some action."
+    Write-Warning "Activate any flag (-Build | -Test | -Publish) to perform some action."
 
     exit 0
 }
 
-$Solution = Join-Path $PSScriptRoot "Code" "LineLengthGuard.sln"
+$SolutionDirectory = Join-Path $PSScriptRoot "Code"
+$Solution = Join-Path $SolutionDirectory "LineLengthGuard.sln"
 
 if ($Build)
 {
@@ -36,4 +40,13 @@ if ($Build)
 if ($Test)
 {
     dotnet test "$Solution" -c "$Configuration" --no-build --logger trx --results-directory "$TestResultsDirectory"
+}
+
+if ($Publish)
+{
+    $packageProject = Join-Path $SolutionDirectory "Source" "LineLengthGuard.Package" "LineLengthGuard.Package.csproj"
+    $PublicationDirectory = [System.IO.Path]::GetFullPath($PublicationDirectory)
+
+    dotnet build "$packageProject" -c "$Configuration" `
+        /p:GeneratePackageOnBuild=true /p:PackageVersion=$Version /p:PackageOutputPath=$PublicationDirectory
 }
