b9bbc71cf5b22c034a16d43691f95e166b094a72
a1ex9788 <alejandrocarrion1999@gmail.com>
2024-11-24 16:47:25 +0100

    Style: Suprimir issue aparecida al utilizar la SDK de .NET 9.

--- Code/Tests/LineLengthGuard.Tests/GlobalSuppressions.cs
+++ Code/Tests/LineLengthGuard.Tests/GlobalSuppressions.cs
@@ -8,6 +8,12 @@ using System.Diagnostics.CodeAnalysis;
     Target = "~M:LineLengthGuard.Tests.IntegrationTests.LLG001IntegrationTests" +
         ".LLG001_LinesLongerThanMaximumLength_ReportsExpectedDiagnostics~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage(
+    "Maintainability",
+    "CA1515:Consider making public types internal",
+    Justification = "Tests framework only detect tests in public classes.",
+    Scope = "namespaceAndDescendants",
+    Target = "~N:LineLengthGuard.Tests")]
+[assembly: SuppressMessage(
     "Minor Code Smell",
     "S100:Methods and properties should be named in PascalCase",
     Justification = "JSON is an acronym.",
